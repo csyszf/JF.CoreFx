3161bdc48e9f697a74a6a6c94ebd8ae477feca77
csyszf <csyszf@outlook.com>
2017-09-22 18:01:39 +0800

    command result for error

--- src/JF.Domain.Abstractions/Command/CommandResult.cs
+++ src/JF.Domain.Abstractions/Command/CommandResult.cs
@@ -6,30 +6,34 @@ namespace JF.Domain.Command
 {
     public class CommandResult
     {
-        public static CommandResult Ok => new CommandResult();
-        public static CommandResult Error(string errorMessage) => new CommandResult(errorMessage);
+        public static CommandResult Ok => new CommandResult { Succeed = true };
+        public static CommandResult Error(string errorMessage) =>
+            new CommandResult
+            {
+                Succeed = false,
+                ErrorMessage = errorMessage
+            };
 
-        public CommandResult()
-        { }
-        public CommandResult(string errorMessage)
-        {
-            ErrorMessage = errorMessage;
-            Succeed = false;
-        }
-        public bool Succeed { get; private set; } = true;
-        public string ErrorMessage { get; private set; }
+        public bool Succeed { get; protected set; }
+        public string ErrorMessage { get; protected set; }
     }
 
-    public class CommandResult<TPayload>: CommandResult
+    public class CommandResult<TPayload> : CommandResult
     {
         public new static CommandResult<TPayload> Ok(TPayload payload) =>
-            new CommandResult<TPayload>(payload);
+            new CommandResult<TPayload>
+            {
+                Succeed = true,
+                Payload = payload
+            };
 
-        public CommandResult(TPayload payload)
-        {
-            Payload = payload;
-        }
+        public new static CommandResult<TPayload> Error(string errorMessage) =>
+            new CommandResult<TPayload>
+            {
+                Succeed = false,
+                ErrorMessage = errorMessage
+            };
 
-        public TPayload Payload { get; set; }
+        public TPayload Payload { get; private set; }
     }
 }
