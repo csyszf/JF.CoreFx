811b600aedabf8291ea7c9bc86fff61c7b853009
csyszf <csyszf@outlook.com>
2017-09-26 11:52:54 +0800

    make CommandResult Euatable

--- src/JF.Domain.Abstractions/Command/CommandResult.cs
+++ src/JF.Domain.Abstractions/Command/CommandResult.cs
@@ -4,9 +4,9 @@ using System.Text;
 
 namespace JF.Domain.Command
 {
-    public class CommandResult
+    public class CommandResult : IEquatable<CommandResult>
     {
-        public static CommandResult Ok => new CommandResult { Succeed = true };
+        public static CommandResult Ok = new CommandResult { Succeed = true };
         public static CommandResult Error(string errorMessage) =>
             new CommandResult
             {
@@ -16,13 +16,28 @@ namespace JF.Domain.Command
 
         public bool Succeed { get; protected set; }
         public string ErrorMessage { get; protected set; }
+
+        public virtual bool Equals(CommandResult other)
+        {
+            if (Succeed)
+                return other.Succeed;
+            else
+                return (!other.Succeed && ErrorMessage == ErrorMessage);
+        }
     }
 
-    public class CommandResult<TPayload> : CommandResult
+    public class CommandResult<TPayload> : CommandResult, IEquatable<CommandResult<TPayload>>
+        where TPayload : IEquatable<TPayload>
     {
         public TPayload Payload { get; protected set; }
+
+        public virtual bool Equals(CommandResult<TPayload> other)
+        {
+            if (Succeed)
+                return (other.Succeed && Payload.Equals(other.Payload));
+            else
+                return (!other.Succeed && ErrorMessage == ErrorMessage);
+        }
     }
-
-
 
 }
